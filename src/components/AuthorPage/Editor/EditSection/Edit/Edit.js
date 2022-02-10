7e9f1b4ff4c9e5ceea626a92c6738027c8a6e2fe
Aryan Mishra <mishra321123@gmail.com>
2022-02-10 15:12:22 +0530

    added create Post route and creation logics

--- src/components/AuthorPage/Editor/EditSection/Edit/Edit.js
+++ src/components/AuthorPage/Editor/EditSection/Edit/Edit.js
@@ -1,4 +1,6 @@
 import { createContext, useState, useContext } from 'react';
+import axios from 'axios';
+
 import uuid from 'react-uuid';
 import Text from "./Text/Text";
 import Image from "./Image/Image";
@@ -14,6 +16,9 @@ import { MdDoneOutline } from 'react-icons/md';
 import { BsImage, BsCardText } from 'react-icons/bs';
 import { VscAdd } from 'react-icons/vsc';
 import { GrSend } from 'react-icons/gr';
+import { FcApproval, FcLink } from 'react-icons/fc';
+import { MdError } from 'react-icons/md';
+import { GiEmptyWoodBucketHandle } from 'react-icons/gi';
 import { TiCancelOutline } from 'react-icons/ti';
 
 //CSS
@@ -30,9 +35,16 @@ function Edit(props){
 
     const [content, setContent] = useState(props.content || []);
     const [titleEdit, setTitleEdit] = useState(true);
-    const [mainTitle, setMainTitle] = useState('');
+    const [mainTitle, setMainTitle] = useState('Title of the Blog');
     const [post, setPost] = useState(false);
+    const [posted, setPosted] = useState(false);
     const [append, setAppend] = useState(false);
+    const [reset, setReset] = useState(false);
+    const [retry, setRetry] = useState(false);
+    const [emptyBlog, setEmptyBlog] = useState(false);
+    const [blogLink, setBlogLink] = useState('');
+    const [linkCopied, setLinkCopied] = useState(false);
+    const [uploadPercentage, setUploadPercentage] = useState(0);
 
     function addContent(ContentType){
         let newContent = {
@@ -43,6 +55,52 @@ function Edit(props){
         setContent([...content, newContent])
     }
 
+
+
+    async function PostToServer(){
+        if(content.length === 0){
+            setEmptyBlog(true);
+            return;
+        }
+        setRetry(false);
+        setPost(true);
+        setEmptyBlog(false);
+
+        
+
+        try{
+            const res = await axios.post('http://localhost:5000/AuthorPage/updateBlog', {
+                title : mainTitle,
+                content : content
+            }, {
+                onUploadProgress : progressEvent => {
+                    setUploadPercentage(parseInt(Math.round((progressEvent.loaded * 100) / progressEvent.total)))
+            }});
+
+            if (res.data.posted) {
+                var titleString = mainTitle;
+                for(let i = 0; i<mainTitle.length; i++){
+                    if(titleString[i] === ' '){
+                        titleString[i] = '+';
+                    }
+                }
+                let link = `http://localhost:5000/?blog=${titleString}`;
+                console.log(link);
+                setBlogLink(link);
+                setPosted(true);
+            }
+        }
+        catch(err){
+            console.log(err);
+            setPost(false);
+            setRetry(true);    
+        }
+    }
+
+
+
+
+
     return(
         <section className='Playground'>
             
@@ -51,72 +109,126 @@ function Edit(props){
                 <TiCancelOutline size="2rem" className='UpdateToggler' onClick={() => setEdit(false)}/>
             </div>
 
-            <div className="AddTitle">
-                <button className="TitleControlBtn" onClick={() => setTitleEdit(!titleEdit)}>{titleEdit?<MdDoneOutline size="1.2rem" />:<FiEdit2 size="1.2rem" />}</button>
-                <h1 className={titleEdit?'MainTitleHide':'MainTitle'}>{mainTitle}</h1>
-                <input type="text" className={titleEdit?"TitleInput":"TitleInputHide"} placeholder="Title Here..." value={mainTitle} onChange={e => setMainTitle(e.target.value)}></input>
-            </div>
+            <div className='BlogContent'>
 
-            <div className="AddContent">
-            
-                <p className="ContentLabel">Pen Down</p>
+                {posted ? <div className="PostedBanner">
+                    <FcApproval id='PostedIcon' size="7rem" color='green'/>
+                    <h1>Updated!!</h1>
+                </div> : null}
 
-                <EditSectionContext.Provider value={{ content, setContent, append, setAppend}}>
-                    <div className="ContentArea">
-                        {
-                            content.map((c, index) => {
-                                if(c){
-                                    if(c.type === "p"){
-                                        return(
-                                            <div className="Content-Append-Area">
-                                                <Append ContentID={index} key={uuid()}/>
-                                                <Text ContentID={index} key={c.key}/>
-                                            </div>
-                                        )
-                                    }
-                                    else if(c.type === "i"){
-                                        return(
-                                            <div className="Content-Append-Area">
-                                                <Append ContentID={index} key={uuid()}/>
-                                                <Image ContentID={index} key={c.key}/>
-                                            </div>
-                                        )
-                                    }
-                                    else if(c.type === 'v'){
-                                        return(
-                                            <div className="Content-Append-Area">
-                                                <Append ContentID={index} key={uuid()}/>
-                                                <Video ContentID={index} key={c.key}/>
-                                            </div>
-                                        )
-                                    }
-                                    else if(c.type === 'n'){
-                                        return(
-                                            <div className="Content-Append-Area" key={uuid()}>
-                                                <p>Select Content-Type from Content-Selector Section.</p>
-                                            </div>
-                                        )
-                                    }
-                                }
-                                return(
-                                    <p key={uuid()}>NoContent</p>
-                                )
-                            })
-                        }
-                    </div>
-                </EditSectionContext.Provider>
 
-                <div className="ContentTypeBtnsArea">
-                    <button className="ContentBtn" onClick={() => addContent('p')}><BsCardText size="2rem" /></button>
-                    <button className="ContentBtn" onClick={() => addContent('i')}><BsImage size="2rem" /></button>
-                    <button className="ContentBtn" onClick={() => addContent('v')}><FiYoutube size="2rem" /></button>
-                    <button className='ContentBtn' onClick={() => setAppend(!append)}><VscAdd/>Append</button>
-
+                <div className="AddTitle">
+                    <button className="TitleControlBtn" onClick={() => setTitleEdit(!titleEdit)}>{titleEdit?<MdDoneOutline size="1.2rem" />:<FiEdit2 size="1.2rem" />}</button>
+                    <h1 className={titleEdit?'MainTitleHide':'MainTitle'}>{mainTitle}</h1>
+                    <input type="text" className={titleEdit?"TitleInput":"TitleInputHide"} placeholder="Title Here..." value={mainTitle} onChange={e => setMainTitle(e.target.value)} />
                 </div>
+
+                <div className="AddContent">
+                
+                    <p className="ContentLabel">Pen Down</p>
+
+                    <EditSectionContext.Provider value={{ content, setContent, append, setAppend}}>
+                        <div className="ContentArea">
+                            {
+                                content.map((c, index) => {
+                                    if(c){
+                                        if(c.type === "p"){
+                                            return(
+                                                <div className="Content-Append-Area">
+                                                    <Append ContentID={index} key={uuid()}/>
+                                                    <Text ContentID={index} key={c.key}/>
+                                                </div>
+                                            )
+                                        }
+                                        else if(c.type === "i"){
+                                            return(
+                                                <div className="Content-Append-Area">
+                                                    <Append ContentID={index} key={uuid()}/>
+                                                    <Image ContentID={index} key={c.key}/>
+                                                </div>
+                                            )
+                                        }
+                                        else if(c.type === 'v'){
+                                            return(
+                                                <div className="Content-Append-Area">
+                                                    <Append ContentID={index} key={uuid()}/>
+                                                    <Video ContentID={index} key={c.key}/>
+                                                </div>
+                                            )
+                                        }
+                                        else if(c.type === 'n'){
+                                            return(
+                                                <div className="Content-Append-Area" key={uuid()}>
+                                                    <p>Select Content-Type from Content-Selector Section.</p>
+                                                </div>
+                                            )
+                                        }
+                                    }
+                                    return(
+                                        <p key={uuid()}>NoContent</p>
+                                    )
+                                })
+                            }
+                        </div>
+                    </EditSectionContext.Provider>
+
+                    <div className="ContentTypeBtnsArea">
+                        <button className="ContentBtn" onClick={() => addContent('p')}><BsCardText size="2rem" /></button>
+                        <button className="ContentBtn" onClick={() => addContent('i')}><BsImage size="2rem" /></button>
+                        <button className="ContentBtn" onClick={() => addContent('v')}><FiYoutube size="2rem" /></button>
+                        <button className='ContentBtn' onClick={() => setAppend(!append)}><VscAdd/>Append</button>
+
+                    </div>
+                </div>
+                    
             </div>
+
+
+
+            <section className='ContentControlMsgArea'>
+                {retry ? <div id='RetryMsgArea' className='MsgArea'>
+                    <MdError size='5rem' className='MsgIcon'/>
+                    <p className='msg'>Some error Occurred!, Please Retry.</p>
+                </div> : null}
+
+                {emptyBlog?<div className="MsgArea" id='EmptyBlogArea'>
+                    <GiEmptyWoodBucketHandle size="5rem" id='EmptyBlogIcon' className='MsgIcon'/>
+                    <p className='msg'>Can't Post Empty Blog. Please Write Something!</p>
+                </div> : null}
+
+                {reset?<div className='ResetArea'>
+                    <h1>Are you sure you wan't to Clear All? Once Cleared, CANNOT BE UNDONE!!!</h1>
+                    <div id="ResetConfirmationBtnsArea">
+                        <button id='ResetY' onClick={() => {
+                            setContent([]);
+                            setReset(!reset);
+                            setRetry(false);
+                            setEmptyBlog(false);
+                            setMainTitle('');
+                            setTitleEdit(true);
+                        }}>Yes</button>
+                        <button id='ResetN' onClick={() => setReset(!reset)}>No</button>
+                    </div>
+                </div>:null}
+                
+                {posted?<div className='MsgArea' id='ShareBlog'>
+                    <p className='msg' id='ShareBlogMsg'>
+                        Share your blog.
+                    </p>
+                    <button className='MsgIcon' id='ShareBlogButton' onClick={() => {
+                        navigator.clipboard.writeText(blogLink);
+                        setLinkCopied(true);
+                    }}><FcLink size='3rem'/>{linkCopied?"Copied":"Link"}</button>
+                </div> : null}
+
+                {post ? <div className='MsgArea' id='UploadedArea'>
+                    <p className='msg' id='UploadPercentage'>Uploaded {posted ? 100 : uploadPercentage}</p>
+                </div> : null}
+
+            </section>
 
             <div className="FinalisingBtns">
-                <button className='FinalisingBtn' onClick={() => setPost(!post)}><GrSend size="2rem"/>Update</button>
+                <button className='FinalisingBtn' onClick={() => PostToServer()}><GrSend size="2rem" id='UpdateBtnIcon'/>{retry ? "Retry" : post?(posted?"Updated!":"Updating..."):"Update"}</button>
             </div>
 
         </section>
